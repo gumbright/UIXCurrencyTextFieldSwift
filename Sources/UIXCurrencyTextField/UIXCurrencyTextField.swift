534496e281ad127a4396af62d161eb14d0f7d667
Guy Umbright <gumbright@gmail.com>
2020-03-17 20:37:16 -0500

    checkpoint

--- Sources/UIXCurrencyTextField/UIXCurrencyTextField.swift
+++ Sources/UIXCurrencyTextField/UIXCurrencyTextField.swift
@@ -11,6 +11,11 @@ protocol UIXCurrencyTextFieldDelegate
 
 class UIXCurrencyTextField : UIView,UITextFieldDelegate
 {
+    static  formatter = {
+        let f = NumberFormatter()
+        f.numberStyle = .currency
+        return f
+    }
     var textColor : UIColor = UIColor.black
     var font : UIFont?
     var caretWidth : CGFloat = 3.0
@@ -19,6 +24,25 @@ class UIXCurrencyTextField : UIView,UITextFieldDelegate
     var value : float
     var decimalValue : NSDecimalNumber
     var delegate : UIXCurrencyTextFieldDelegate
+    
+    func defaultAccessoryView() -> UIView
+    {
+        let v = UIToolbar()
+        v.barTintColor = UIColor(colorWithWhite:.85, alpha:0.5)
+        v.items = [UIBarButtonItem(]
+    }
+    - (void) setupDefaultAccessoryView
+    {
+        UIToolbar* v = [[UIToolbar alloc] initWithFrame:CGRectZero];
+        v.barTintColor = [UIColor colorWithWhite:.85 alpha:.5];
+        
+        v.items = @[[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                    [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(donePressed:)]];
+        [v sizeToFit];
+        
+        self.currentInputAccessory = v;
+    }
+
 }
 //@property (nonatomic, readwrite) UIColor* textColor;
 //@property (nonatomic, readwrite) UIFont* font;
